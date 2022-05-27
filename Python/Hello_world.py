c8b8b757e8b44124efed8dda1e5bdb7c44c2d742
Datamaven1 <101432050+DataMaven1@users.noreply.github.com>
2022-05-27 01:12:05 +0100

    hello

---
+++ Python/Hello_world.py
@@ -0,0 +1,3 @@
+# A simple python program that output "Hello World" on my terminal
+
+print("Hello World!")
---
+++ Python/Words_counter.py
@@ -0,0 +1,11 @@
+# A python program that counts and return the number of words in a givrn string
+
+text = "This is my first python program"
+
+#using split() function
+
+res = text.split(" ")
+
+#total number of words
+
+print(len(res))
