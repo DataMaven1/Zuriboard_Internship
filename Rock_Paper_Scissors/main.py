a6340677e2f193060f15a7a753b0d66c6a0a5994
Datamaven1 <101432050+DataMaven1@users.noreply.github.com>
2022-06-13 23:41:36 +0100

    Rock_Paper_Scissors

---
+++ Rock_Paper_Scissors/main.py
@@ -0,0 +1,46 @@
+#A Simple program to create the Rock-Paper-Scissors game in Python
+#Rock-Paper-Scissors is a simple two-player game
+#one player will be controlled by the computer and the other player controlled by you - the user (i.e CPU vs Player).
+
+import random
+
+game_over = False
+
+while not game_over:
+    possible_choices = ["R", "P", "S"]
+    player_choice = input("Welcome, Enter your Choice:\n")
+    computer_choice = random.choice(possible_choices)
+
+    print(f"Player: {player_choice}, Computer: {computer_choice}\n")
+
+    if player_choice not in possible_choices:
+        print("Invalid input, Try again\n")
+        continue
+
+
+    if player_choice == computer_choice:
+        print(f"Both player select {player_choice}, Its a tie\n")
+
+    elif player_choice == "R":
+        if computer_choice == "S":
+            print("Rock smashes Scissors, You Won\n")
+        else:
+            print("Oh no!, You lose")
+            continue
+        break
+
+    elif player_choice == "S":
+        if computer_choice == "P":
+            print("Scissors cut paper, You won\n")
+        else:
+            print("oh no, You lose\n")
+            continue
+        break
+
+    elif player_choice == "P":
+        if computer_choice == "R":
+            print("Paper cover rock, You won")
+        else:
+            print("You lose")
+            continue
+        break
